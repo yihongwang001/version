a7ea97a17afe4ea6978cbd553dea8584ac93f821
Yihong <71856144+yihongwang001@users.noreply.github.com>
2020-11-24 16:51:27 -0800

    Update Register.js

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import loginP from "../images/login.png";import "./textbox.css";
+import loginP from "../images/login.png";
 
 import { useState } from "react";
 import { Form, Button } from "react-bootstrap";
